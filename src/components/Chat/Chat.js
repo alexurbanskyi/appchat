34b191e9522fe3602b21af8ed89946ebe31012ff
roman_shvets <roman.shvets@transfotech.com>
2023-02-22 17:35:06 +0200

    fix sort messages

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,15 +1,33 @@
 import React, { useEffect, useState } from "react";
 import "./chat.css";
 
-import { addDoc, collection, serverTimestamp, onSnapshot, query, where} from "firebase/firestore";
+import {
+  addDoc,
+  collection,
+  serverTimestamp,
+  onSnapshot,
+  query,
+  where,
+  orderBy,
+} from "firebase/firestore";
 import { db, auth } from "../../firebase-config";
 
-function Chat({room}) {
+function Chat({ room }) {
   const [newMessage, setNewMessage] = useState("");
-  const messageRef = collection(db, 'messages');
+  const messageRef = collection(db, "messages");
+  const [messages, setMessages] = useState([]);
+
   useEffect(() => {
-    const queryMessages = query(messageRef, where('room', '==', room) )
-    onSnapshot(queryMessages, () => {console.log('You Have New Messages')});
+    const queryMessages = query(messageRef, where("room", "==", room), orderBy('createdAt'));
+    const unsuscribe = onSnapshot(queryMessages, (snapshot) => {
+        let messagesArr = [];
+      snapshot.forEach((doc) => {
+        messagesArr.push({ ...doc.data(), id: doc.id });
+      });
+      console.log("You Have New Messages", messagesArr );
+      setMessages(messagesArr)
+    });
+    return () => unsuscribe()
   }, []);
 
   async function handelSubmit() {
@@ -17,16 +35,19 @@ function Chat({room}) {
     if (newMessage === "") return;
 
     await addDoc(messageRef, {
-        text: newMessage,
-        createdAt: serverTimestamp(),
-        user: auth.currentUser.displayName,
-        room: room
-
+      text: newMessage,
+      createdAt: serverTimestamp(),
+      user: auth.currentUser.displayName,
+      room: room,
     });
-    setNewMessage('');
+    setNewMessage("");
   }
   return (
     <div className="chat">
+        <h1>Welcome to: {room}</h1>
+        {
+            messages.map((mess) => <div key={mess.id}>{mess.text}</div> )
+        }
       <input
         placeholder="type your message..."
         onChange={(e) => setNewMessage(e.target.value)}
